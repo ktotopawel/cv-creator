51727f2c8436600f4cbc822353ac47402ed41ddc
ktotopawel <pawel.juszczak02@gmail.com>
2025-05-11 17:18:16 +0200

    added download button

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -4,6 +4,8 @@ import Heading from "./Heading";
 import Skills from "./Skills";
 import Work from "./Work";
 import "../../styles/Preview.css";
+import { usePDF } from "react-to-pdf";
+import downloadIcon from "../../assets/download.svg";
 
 type FormProps = {
   personalData: PersonalType;
@@ -16,17 +18,26 @@ export default function Preview({
   schools,
   workplaces,
 }: FormProps) {
+  const { toPDF, targetRef } = usePDF({
+    filename: `${personalData.firstName.toLowerCase()}-${personalData.lastName.toLowerCase()}.pdf`,
+  });
+
   return (
     <>
-      <Heading personalData={personalData}></Heading>
-      <div className="previewContent">
-        <Education schools={schools}></Education>
-        <Work workplaces={workplaces}></Work>
-        <Skills
-          softSkills={personalData.softSkills}
-          hardSkills={personalData.hardSkills}
-          languages={personalData.languages}
-        ></Skills>
+      <button onClick={() => toPDF()} className="downloadBtn">
+        <img src={downloadIcon} alt="download" />
+      </button>
+      <div ref={targetRef}>
+        <Heading personalData={personalData}></Heading>
+        <div className="previewContent">
+          <Education schools={schools}></Education>
+          <Work workplaces={workplaces}></Work>
+          <Skills
+            softSkills={personalData.softSkills}
+            hardSkills={personalData.hardSkills}
+            languages={personalData.languages}
+          ></Skills>
+        </div>
       </div>
     </>
   );
